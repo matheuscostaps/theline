25964b9d379cd745191478949844d171a951fbc7
Taís Döring <taisdoring06@gmail.com>
2026-04-01 11:59:44 -0300

    Update BDTheLine.sql

--- Banco de Dados/BDTheLine.sql
+++ Banco de Dados/BDTheLine.sql
@@ -14,7 +14,7 @@ CREATE TABLE clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     cpf VARCHAR(14) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     telefone VARCHAR(20),
     endereco TEXT
 );
