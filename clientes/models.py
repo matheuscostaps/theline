f97d88c997db52971de72f491620b1a40d525d14
Geovana Rodrigues <geovana202110rod@gmail.com>
2026-05-05 21:02:41 -0300

    commit 2

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    telefone = models.CharField(max_length=20, null=True, blank=True)
+    endereco = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
