8b82189841c615119ead5ad40423c0ef4cb9ad58
Matheus Costa <matheuscostaps95@gmail.com>
2026-05-06 20:04:25 -0300

    Update views.py

--- vendas/views.py
+++ vendas/views.py
@@ -5,3 +5,20 @@ from .serializers import VendaSerializer
 class VendaViewSet(viewsets.ModelViewSet):
     queryset = Venda.objects.all()
     serializer_class = VendaSerializer
+
+    @action(detail=False, methods=['get'])
+    def relatorio(self, request):
+        # 1. Puxa a soma total da coluna 'valor_total' do banco
+        dados_banco = Venda.objects.aggregate(
+            faturamento_total=Sum('valor_total'),
+            ticket_medio=Avg('valor_total'),
+            total_vendas=Count('id')
+        )
+
+        return Response({
+            "status": "sucesso",
+            "faturamento_total": dados_banco['faturamento_total'] or 0,
+            "ticket_medio": dados_banco['ticket_medio'] or 0,
+            "quantidade_vendas": dados_banco['total_vendas'],
+            "mensagem": "Dados extraídos em tempo real do banco de dados"
+        })
