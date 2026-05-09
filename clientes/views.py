eb7e8b30265a3c84bea1d58e4ec00ffb7f8e7906
Taís Döring <taisdoring06@gmail.com>
2026-05-09 14:28:04 -0300

    Delete clientes directory

--- clientes/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from .models import Cliente
-from .serializers import ClienteSerializer
-
-class ClienteViewSet(viewsets.ModelViewSet):
-    queryset = Cliente.objects.all()
-    serializer_class = ClienteSerializer
