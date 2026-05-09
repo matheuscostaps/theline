d03b89b74b69bc6de4a3208f5c24bde6772893de
Taís Doring Freire da Silva <tais.doring@dinfor.uniceub>
2026-05-09 12:27:14 -0300

    Salvando minhas alterações antes de atualizar

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from theline.views import ClienteViewSet
+
+from theline.views import (
+    ClienteViewSet,
+    ProdutoViewSet,
+    VendaViewSet
+)
 
 router = DefaultRouter()
-router.register(r'clientes', ClienteViewSet) 
+
+router.register(r'clientes', ClienteViewSet)
+router.register(r'produtos', ProdutoViewSet)
+router.register(r'vendas', VendaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('', include('theline.urls')), 
+    path('', include('theline.urls')),
 ]
