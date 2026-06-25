794d4c0b139556c69b7dfe951e450071001bc11b
Alysson Kennedy de Oliveira Carvalho <alysson.kennedy@dinfor.uniceub>
2026-06-25 09:33:08 -0300

    Message fade out

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8)kges&pm^$ctwk!ivpyi&1!$pc!eyv8k32w44=4_+cxh0#ofe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
